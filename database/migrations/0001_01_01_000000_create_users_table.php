f27bf3d09e25eed26945f2a0e814db1129410b8f
nherlhinguevarra <guevarra.nherlhin13@gmail.com>
2025-03-05 08:59:54 +0800

    migrations for login and products

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,8 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable(); // wont need password if google acc is used
-            $table->string('google_id')->nullable();
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
